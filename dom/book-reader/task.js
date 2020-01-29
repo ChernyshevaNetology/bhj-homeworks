50bcd4a23524ce4ea4f76cdafecab84088c63ef0
unknown <C:\Users\Andrey\AppData\Roaming\The Bat!>
2020-01-29 23:01:47 +0200

    правки

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,128 +1,77 @@
 const fontSize = document.querySelector('.book__control_font-size');
 const fontColor = document.querySelector('.book__control_color');
 const BGcolor = document.querySelector('.book__control_background')
-const BGoptions = document.querySelectorAll('.book__control_background .color');
-const els = document.querySelectorAll('.font-size');
-const colorOptions = document.querySelectorAll('.book__control_color .color');
 const book = document.getElementById('book');
 const blackBG = document.querySelector('.book__control_background .color_black');
 const grayBG = document.querySelector('.book__control_background .color_gray');
 const blackFont = document.querySelector('.book__control_color .color_black');
-const grayFont = document.querySelector('.book__control_color .color_gray')
+const grayFont = document.querySelector('.book__control_color .color_gray');
+let activeFontAtr = book.querySelector(".font-size_active").getAttribute("data-size");
+let activeColorAtr = book.querySelector(".color_active").getAttribute("data-color");
+let activeColorBG = book.querySelector(".bg_active").getAttribute("data-bg");
 
 function setFont(e) {
+    const fontActive = book.querySelector('.font-size_active');
     // при клике на активный класс выходим из функции, так как ничего делать не надо.
     e.preventDefault();
     if (e.target.classList.contains('font-size_active')) {
         return;
     }
     // убираем класс у активного элемента
-    els.forEach(el => el.classList.remove('font-size_active'));
-
+    fontActive.classList.remove('font-size_active');
     //добавляем класс кликнутому элементу
     e.target.classList.add('font-size_active');
 
     // меняем шрифт в зависимости от значения атрибута data-size
-    if (e.target.getAttribute("data-size") === 'small') {
-        book.classList.remove('book_fs-big');
-        book.classList.add('book_fs-small');
-    }
+    if (activeFontAtr) book.classList.remove(`book_fs-${activeFontAtr}`);
 
-    if (e.target.getAttribute("data-size") === 'big') {
-        book.classList.remove('book_fs-small');
-        book.classList.add('book_fs-big');
-    }
+    activeFontAtr = e.target.getAttribute("data-size");
 
-    if (!e.target.getAttribute("data-size")) {
-        book.classList.remove('book_fs-big') || book.classList.remove('book_fs-small');
-    }
+    if (activeFontAtr) book.classList.add(`book_fs-${activeFontAtr}`);
 }
 
 function setFontColor(e) {
+    const colorActive = book.querySelector('.color_active');
+
     // при клике на активный класс выходим из функции, так как ничего делать не надо.
     e.preventDefault();
     if (e.target.classList.contains('color_active') || e.target.textContent) {
         return;
     }
     // убираем класс у активного элемента
-    colorOptions.forEach(el => el.classList.remove('color_active'));
-
+    colorActive.classList.remove('color_active');
     //добавляем класс кликнутому элементу
     e.target.classList.add('color_active');
 
     // меняем цвет в зависимости от значения атрибута data-color
-    if (e.target.getAttribute("data-color") === 'gray') {
-        book.classList.remove('book_color-whitesmoke')
-        book.classList.add('book_color-gray');
-    }
+    if (activeColorAtr) book.classList.remove(`book_color-${activeColorAtr}`);
 
-    if (e.target.getAttribute("data-color") === 'whitesmoke') {
-        book.classList.remove('book_color-gray')
-        book.classList.add('book_color-whitesmoke');
-    }
+    activeColorAtr = e.target.getAttribute('data-color');
 
-    if (!e.target.getAttribute("data-color")) {
-        book.classList.remove('book_color-gray') || book.classList.remove('book_color-whitesmoke');
-    }
+    if (activeColorAtr) book.classList.add(`book_color-${activeColorAtr}`);
 }
 
 function setBGcolor(e) {
+    const BGactive = book.querySelector('.bg_active');
     // при клике на активный класс выходим из функции, так как ничего делать не надо.
     e.preventDefault();
-    if (e.target.classList.contains('color_active') || e.target.textContent) {
+    if (e.target.classList.contains('bg_active') || e.target.textContent) {
         return;
     }
-
     // убираем класс у активного элемента
-    BGoptions.forEach(el => el.classList.remove('color_active'));
-
+    BGactive.classList.remove('bg_active');
     //добавляем класс кликнутому элементу
-    e.target.classList.add('color_active');
+    e.target.classList.add('bg_active');
 
-    // меняем фон в зависимости от значения атрибута data-color
-    if (e.target.getAttribute("data-color") === 'black') {
-        book.classList.remove('color_gray')
-        book.classList.add('color_black');
-    }
+    if (activeColorBG) book.classList.remove(`book_bg-${activeColorBG}`);
 
-    if (e.target.getAttribute("data-color") === 'gray') {
-        book.classList.remove('color_black');
-        book.classList.add('color_gray');
-    }
-    if (!e.target.getAttribute("data-color")) {
-        book.classList.remove('color_gray') || book.classList.remove('color_black');
-    }
-}
+    activeColorBG = e.target.getAttribute('data-bg');
 
-function checkMatch() {
-    if (!book.classList.contains('book_color-gray') && !book.classList.contains('book_color-whitesmoke')) {
-        blackBG.style = "display: none";
-    } else {
-        blackBG.style = "display: block";
-    }
-
-    if (book.classList.contains('book_color-gray')) {
-        grayBG.style = "display: none"
-    } else {
-        grayBG.style = "display: block"
-    }
-
-    if (book.classList.contains('color_gray')) {
-        grayFont.style = "display: none";
-    } else {
-        grayFont.style = "display: block";
-    }
-
-    if (book.classList.contains('color_black')) {
-        blackFont.style = "display: none";
-    } else {
-        blackFont.style = "display: block";
-    }
+    if (activeColorBG) book.classList.add(`book_bg-${activeColorBG}`);
 }
 
 
-document.addEventListener("DOMContentLoaded", checkMatch)
+
 fontSize.addEventListener('click', setFont);
 fontColor.addEventListener('click', setFontColor);
 BGcolor.addEventListener('click', setBGcolor);
-book.addEventListener('click', checkMatch);
